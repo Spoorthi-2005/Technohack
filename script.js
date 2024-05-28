b71b4392599952287b65659183d9f06d7e14e7b7
Spoorthi-2005 <155543462+Spoorthi-2005@users.noreply.github.com>
2024-05-28 14:51:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+// script.js
+
+function inputValue(value) {
+    const display = document.getElementById('display');
+    if (display.innerText === '0') {
+        display.innerText = value;
+    } else {
+        display.innerText += value;
+    }
+}
+
+function clearDisplay() {
+    document.getElementById('display').innerText = '0';
+}
+
+function deleteLast() {
+    const display = document.getElementById('display');
+    display.innerText = display.innerText.slice(0, -1);
+    if (display.innerText === '') {
+        display.innerText = '0';
+    }
+}
+
+function calculateResult() {
+    const display = document.getElementById('display');
+    try {
+        display.innerText = eval(display.innerText);
+    } catch {
+        display.innerText = 'Error';
+    }
+}
